df6555ca14e262e172749cd49db9a7c7357dec2b
Evgeny Tanhilevich <evgeny.tanhilevich@gmail.com>
2014-01-13 11:18:00 +0000

    Set up guard and spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,4 +41,18 @@ describe "StaticPages" do
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
+
+ describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+
 end
