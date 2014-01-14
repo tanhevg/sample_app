4399bff07cc543500f54cc8110643e16cfd1fe6a
Evgeny Tanhilevich <evgeny.tanhilevich@gmail.com>
2014-01-14 22:33:16 +0000

    User signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Evgeny Tanhilevich"
+    email    "tanhevg@example.com"
+    password "qqqzzz"
+    password_confirmation "qqqzzz"
+  end
+end
